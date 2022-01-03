375f6b2e4c9ec42589af61cb9ff4c1105eb0413a
Adrian <adrian.miranda.r@gmail.com>
2022-01-02 22:28:17 -0300

    se agrega enzyme, enzyme-adapter-react-16,enzyme-to-json y se realiza la primera prueba, todo ok

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,11 @@
+import react from "react"
+import { shallow } from "enzyme"
+import { GifGridItem } from "../../components/GifGridItem"
+
+describe('prueba en <GifGridItem />', () => {
+    test('debe mostrar el componente correctamente', () => {
+        const wrapped = shallow(<GifGridItem />);
+        expect(wrapped).toMatchSnapshot();
+    })
+    
+})
